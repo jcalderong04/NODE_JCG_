1e4afda7dbf0fbca3fa47df3d12efa0261dc2693
jcalderong04 <133767521+jcalderong04@users.noreply.github.com>
2024-02-29 11:48:15 +0100

    TODO

---
+++ NODE_EXAMEN/src/controllers/controllers.js
@@ -0,0 +1,3 @@
+console.log('Toda su puta madre que va de categoria');
+
+export const home = (req,res) => res.render('home', {title:'home'})
---
+++ NODE_EXAMEN/src/routes/routes.js
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { home } from '../controllers/controllers.js';
+const router = Router();
+
+
+//Definir rutas
+router.get('/', home);
+
+
+export default router;
